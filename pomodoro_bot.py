7903c3045791762b70ca9bff297681822a135f9e
RhamzThev <rhamzthev@gmail.com>
2022-07-08 17:01:48 -0400

    created bot and added a commnad for bot

--- pomodoro_bot.py
+++ pomodoro_bot.py
@@ -1,2 +1,25 @@
-import discord
+import hikari
+import lightbulb
+import secret
 
+my_intents = hikari.Intents.GUILDS
+
+bot = lightbulb.BotApp(
+    token=secret.Secret.TOKEN,
+    intents=my_intents
+)
+
+# START/END COMMAND
+@bot.command
+@lightbulb.command("start", "Starts the pomodoro timer")
+@lightbulb.implements(lightbulb.SlashCommand)
+
+async def start_timer(ctx: lightbulb.Context) -> None:
+    await ctx.respond("Timer has started!")
+
+# COMMAND TO KNOW TIME REMAINING
+
+# NOTIFICATIONS FOR TIME INTERVALS
+
+
+if __name__ == "__main__": bot.run()
