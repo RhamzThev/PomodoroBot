e85a32cd38139fdef271c750b27b473b27562970
RhamzThev <rhamzthev@gmail.com>
2022-07-11 13:46:01 -0400

    created match cases for commands

--- pomodoro_bot.py
+++ pomodoro_bot.py
@@ -12,6 +12,10 @@ bot = lightbulb.BotApp(
     default_enabled_guilds=[995023091065426011]
 )
 
+class Timer:
+    def __init__(self) -> None:
+        self.status = Status.STOPPED
+
 class Status(Enum):
     RUNNING = 1
     PAUSED = 2
@@ -19,28 +23,34 @@ class Status(Enum):
 
 status = Status.STOPPED
 
-# START/END COMMAND
+# START/STOP COMMAND
 @bot.command
 @lightbulb.command("start", "Starts the pomodoro timer.")
 @lightbulb.implements(lightbulb.SlashCommand)
 
 async def start_timer(ctx: lightbulb.Context) -> None:
-    # match status:
-    #     case Status.RUNNING:
-    #     case Status.PAUSED:
-    #     case Status.STOPPED:
-
-    if status == Status.STOPPED:
-        await ctx.respond("Timer has started!")
-    else:
-        await ctx.respond("You can't start the timer when")
+    global status
+    match status:
+        case Status.RUNNING:
+            await ctx.respond("You can't start the timer when it's running.")
+        case Status.PAUSED:
+            await ctx.respond("You can't start the timer when it's paused. I think `/resume` is what you're looking for.")
+        case Status.STOPPED:
+            status = Status.RUNNING
+            await ctx.respond("Timer has started!")
 
 @bot.command
-@lightbulb.command("end", "Ends the pomodoro timer.")
+@lightbulb.command("stop", "Stops the pomodoro timer.")
 @lightbulb.implements(lightbulb.SlashCommand)
 
-async def end_timer(ctx: lightbulb.Context) -> None:
-    await ctx.respond("Timer has ended.")
+async def Stop_timer(ctx: lightbulb.Context) -> None:
+    global status
+    match status:
+        case Status.STOPPED:
+            await ctx.respond("You can't stop a stopped timer.")
+        case _:
+            status = Status.STOPPED
+            await ctx.respond("Timer has been stopped.")
 
 # PAUSE/RESUME COMMAND
 @bot.command
@@ -48,14 +58,30 @@ async def end_timer(ctx: lightbulb.Context) -> None:
 @lightbulb.implements(lightbulb.SlashCommand)
 
 async def resume_timer(ctx: lightbulb.Context) -> None:
-    await ctx.respond("Timer has resumed!")
+    global status
+    match status:
+        case Status.RUNNING:
+            await ctx.respond("Timer is already running.")
+        case Status.PAUSED:
+            status = Status.RUNNING
+            await ctx.respond("Timer has resumed.")
+        case Status.STOPPED:
+            await ctx.respond("You can't resume when it's stopped. I think `/start` is what you're looking for.")
 
 @bot.command
 @lightbulb.command("pause", "Pauses the timer when running.")
 @lightbulb.implements(lightbulb.SlashCommand)
 
 async def pause_timer(ctx: lightbulb.Context) -> None:
-    await ctx.respond("Timer has paused.")
+    global status
+    match status:
+        case Status.RUNNING:
+            status = Status.PAUSED
+            await ctx.respond("Timer is paused.")
+        case Status.PAUSED:
+            await ctx.respond("Time is already paused.")
+        case Status.STOPPED:
+            await ctx.respond("You can't pause a stopped timer.")
 
 # COMMAND TO KNOW TIME REMAINING
 @bot.command
@@ -63,7 +89,15 @@ async def pause_timer(ctx: lightbulb.Context) -> None:
 @lightbulb.implements(lightbulb.SlashCommand)
 
 async def time_remaining(ctx: lightbulb.Context) -> None:
-    await ctx.respond("There is [TBD] time left.")
+    global status
+    match status:
+        case Status.RUNNING:
+            await ctx.respond("You can't start the timer when it's running.")
+        case Status.PAUSED:
+            await ctx.respond("You can't start the timer when it's paused. I think `/resume` is what you're looking for.")
+        case Status.STOPPED:
+            status = Status.RUNNING
+            await ctx.respond("Timer has started!")
 
 # NOTIFICATIONS FOR TIME INTERVALS
 
